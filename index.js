9dd5eeb410ccf3377c48ee01f96cffc69251209e
Connor Peet <connor@connorpeet.com>
2014-10-06 10:44:49 -0400

    Did a refactor

--- index.js
+++ index.js
@@ -2,133 +2,200 @@ var redis = require('redis');
 var bro = require('brototype');
 var _ = require('lodash');
 
-module.exports = (function (config) {
-    var master;
-    var slaves = {};
+function Fantastico (config) {
+    var connections = [];
 
-    master = new redis.createClient(config.port, config.host, config.options);
-
-    master.ready = false;
-    master.on('error', function (e) {
-        master.ready = false;
-    });
-
-    master.on('connect', function () {
-        master.ready = true;
-    });
-
-    master.send_command('ROLE', [], function (err, role) {
-        if (err || !role) {
-            return;
-        }
-        role = getRole(role);
-        master.role = role;
-
-        role.slaves.forEach(setupSlave);
-    });
-
-    function getMaster() {
-        return master;
-    }
-
-    function getSlave(id) {
-        if (!id)  {
-            var ids = _.keys(slaves);
-            id = _.random(0, ids.length - 1);
-        }
-        return slaves[id] || master;
-    }
-
-    function createMasterClient () {
-        return redis.createClient(config.port, config.host, config.options);
-    }
-
-    function createSlaveClient () {
-        var slave;
-        if (bro(master).doYouEven(role.slaves)) {
-            var i = _.random(0, master.role.slaves - 1);
-            slave = master.role.slaves[i];
-        } else {
-            slave = {host: config.host, port: config.port};
-        }
-
-        return redis.createClient(slave.port, slave.host, config.options);
-    }
-
-    function setupSlave(slave) {
-        var id = slave.host + ':' + slave.port;
-
-        var client = new redis.createClient(slave.port, slave.host, config.options);
-        client.id = id;
-        client.on('error', function (error) {
-            // LOG TO SENTRY
-            client.ready = false;
-
-            clearInterval(client.interval);
-        });
-        client.on('connect', function () {
-            updateSlaveStatus(client);
-
-            client.interval = setInterval(updateSlaveStatus, config.check_interval, client);
-        });
-
-        slaves[id] = client;
-    }
-
-    function updateSlaveStatus(slave) {
-        slave.send_command('ROLE', [], function (err, role) {
-            if (err || !role) {
-                slave.ready = false;
-                return;
-            }
-            role = getRole(role);
-            slave.ready = role.ready;
-        });
-    }
-
-    module.exports = {
-        getMaster: getMaster,
-        getSlave: getSlave,
-        createMasterClient: createMasterClient,
-        createClient: createMasterClient,
-        createSlaveClient: createSlaveClient
+    /**
+     * Adds the given config master to start building the Redis "tree".
+     */
+    self.initialize = function () {
+        self.addConnection(config.port, config.host);
     };
-    return module.exports;
-});
 
+    /**
+     * Gets a redis master.
+     *
+     * @return {Redis.Client}
+     */
+    self.getMaster = function () {
+        return self.findRandom('master', id);
+    };
+
+    /**
+     * Gets a redis slave.
+     *
+     * @return {Redis.Client}
+     */
+    self.getSlave = function (id) {
+        return self.findRandom('slave', id);
+    };
+
+    /**
+     * Finds a random instance of a connection with the given role, or attempts
+     * to get the ID if possible.
+     *
+     * @param  {string} role
+     * @param  {string} id
+     * @return {Redis.Client}
+     */
+    self.findRandom = function (role, id) {
+        if (typeof id !== 'undefined') {
+            var c = _.find(connections, {role: role, id: id});
+
+            if (c) {
+                return c;
+            }
+        }
+
+        return _.sample(_.where(connections, {role: role}), 1).client;
+    };
+
+    /**
+     * Returns a new instance of the "master" redis client.
+     *
+     * @return {Redis.Client}
+     */
+    self.addConnection = function  (port, host) {
+        var connection = {
+            port: port,
+            host: host,
+            id: [port, host].join(':'),
+            options: options || {},
+            ready: false,
+            client: redis.createClient(port, host, config.options || {})
+        };
+
+        // On an error, remove this connection from the array (it's no longer)
+        // working, and set a timeout to try to reestablish it.
+        connection.client.on('error', function (error) {
+            connections = _.reject(connections, {port: port, host: host});
+
+            setTimeout(function () {
+                self.addConnection(port, host);
+            }, config.check_interval);
+        });
+
+        // When it's connected, poll the connection for informations...
+        connection.client.on('connect', function () {
+            pollConnection(connection);
+        });
+
+        connections.push(addConnection);
+    };
+
+    /**
+     * Checks the connection. Sends a ROLE command out.
+     *
+     * @param  {{}} connection
+     */
+    function pollConnection (connection) {
+        connection.send_command('ROLE', [], function (err, role) {
+            // First schedule a new check to run. We want to do this before
+            // checking for an error, to monitor if the connection comes back
+            // into a healthy state.
+            setTimeout(function () {
+                pollConnection(connection);
+            }, config.check_interval);
+
+            // If there's an error, say this connection is no longer ready.
+            if (err) {
+                connection.ready = false;
+                throw err;
+            }
+
+            // If we got slaves back, check each of them to see if we already
+            // have them in our connections. If not, then add the connection!
+            // In this way we kind of build outwards from all masters. If one
+            // master is out of date or we add new connections dynamically,
+            // we can handle that!
+            if (role.slaves) {
+                _.forEach(role.slaves, function (slave) {
+                    if (!_.find(connections, {
+                        host: slave.host,
+                        port: slave.port
+                    })) {
+                        self.addConnection(slave.host, slave.port);
+                    }
+                });
+            }
+
+            // The command was successful, so this connection is ready...
+            connection.ready = true;
+            // Update the record to the record data.
+            _.extend(connection, role);
+        });
+    }
+}
+
+/**
+ * Parses the roll response.
+ *
+ * @param  {{}} response
+ * @return {{}}
+ */
 function getRole (response) {
     var role = {};
     switch (response[0]) {
+        case 'master':
+            role.role = 'master';
+            role.offset = response[1];
 
-    case 'master':
-        role.role = 'master';
-        role.offset = response[1];
+            role.slaves = _.map(response[2],
+                function (slave) {
+                    return {
+                        host: slave[0],
+                        port: slave[1],
+                        offset: slave[2]
+                    };
+            });
+        break;
 
-        role.slaves = _.map(response[2],
-            function (slave) {
-                return {
-                host: slave[0],
-                port: slave[1],
-                offset: slave[2]
-            };
-        });
+        case 'slave':
+            role.role = 'slave';
+            role.master = {host: response[1], port: response[2]};
+            role.status = response[3];
+            role.offset = response[4];
+            role.ready  = (role.status === 'connected');
+        break;
 
-    return role;
-
-    case 'slave':
-        role.role = 'slave';
-        role.master = {host: response[1], port: response[2]};
-        role.status = response[3];
-        role.offset = response[4];
-        role.ready  = (role.status === 'connected');
-    return role;
-
-    case 'setinel':
-        role.role = 'sentinel';
-        role.masters = response[1];
-    return role;
-
-    default:
-    return null;
+        case 'setinel':
+            role.role = 'sentinel';
+            role.masters = response[1];
+        break;
     }
+
+    return role;
 }
+
+module.exports = (function () {
+    var instance = new Fantastico();
+
+    return {
+        /**
+         * Returns the instance of Fantasico (singleton pattern).
+         *
+         * @return {Fantastico}
+         */
+        instance: function () {
+            return instance;
+        },
+        /**
+         * Instantiates and returns a new Fantastico instance.
+         *
+         * @return {Fantastico}
+         */
+        create: function () {
+            return new Fantastico();
+        },
+        /**
+         * Swaps out the singleton instance.
+         *
+         * @param  {*} i
+         * @return {void}
+         */
+        swap: function (i) {
+            instance = i;
+        }
+    };
+})();
+
